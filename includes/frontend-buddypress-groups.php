e863dcb7de54f064532e7123616d893cfdc98b14
r-a-y <r-a-y@gmx.com>
2018-10-06 12:01:30 -0700

    Only show group license if one was selected.

--- includes/frontend-buddypress-groups.php
+++ includes/frontend-buddypress-groups.php
@@ -9,30 +9,11 @@ add_action( 'bp_after_group_home_content', function() {
 		return;
 	}
 
-	add_filter( 'option_cac_cc_default', '_cac_cc_fetch_group_license' );
-	add_filter( 'default_option_cac_cc_default', '_cac_cc_fetch_group_license' );
-
-	cac_cc_get_template_part( 'bp-group-footer' );
-
-	remove_filter( 'option_cac_cc_default', '_cac_cc_fetch_group_license' );
-	remove_filter( 'default_option_cac_cc_default', '_cac_cc_fetch_group_license' );
-} );
-
-/**
- * Fetch the group license, if available.
- *
- * Filters 'option_cac_cc_default'.
- *
- * @since 0.1.0
- *
- * @param  string $retval Default license.
- * @return string
- */
-function _cac_cc_fetch_group_license( $retval ) {
+	// Only show a license if one was set for the group.
 	$license = groups_get_groupmeta( bp_get_current_group_id(), 'cac_cc_license' );
-	if ( ! empty( $license ) ) {
-		return $license;
+	if ( empty( $license ) ) {
+		return;
 	}
 
-	return $retval;
-}
+	cac_cc_get_template_part( 'bp-group-footer' );
+} );
