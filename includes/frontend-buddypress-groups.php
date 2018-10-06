74a3ddf189753d5bb54849d95c652e5e13619e80
r-a-y <r-a-y@gmx.com>
2018-10-06 12:07:43 -0700

    Bring back group filtering license functionality.
    Ensures we can use our license functions, which will automatically fetch
    the group license for use in group template parts.
    
    Mistakenly removed in commit e863dcb.

--- includes/frontend-buddypress-groups.php
+++ includes/frontend-buddypress-groups.php
@@ -22,5 +22,29 @@ add_action( 'bp_after_group_home_content', function() {
 		return;
 	}
 
+	add_filter( 'option_cac_cc_default', '_cac_cc_fetch_group_license' );
+	add_filter( 'default_option_cac_cc_default', '_cac_cc_fetch_group_license' );
+
 	cac_cc_get_template_part( 'bp-group-footer' );
+
+	remove_filter( 'option_cac_cc_default', '_cac_cc_fetch_group_license' );
+	remove_filter( 'default_option_cac_cc_default', '_cac_cc_fetch_group_license' );
 } );
+
+/**
+ * Fetch the group license, if available.
+ *
+ * Filters 'option_cac_cc_default'.
+ *
+ * @since 0.1.0
+ *
+ * @param  string $retval Default license.
+ * @return string
+ */
+function _cac_cc_fetch_group_license( $retval ) {
+	$license = groups_get_groupmeta( bp_get_current_group_id(), 'cac_cc_license' );
+	if ( ! empty( $license ) ) {
+		return $license;
+	}
+	return $retval;
+}
