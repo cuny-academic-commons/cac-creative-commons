6c9df34e6db132954ba49485c01d6848c1d589d0
r-a-y <r-a-y@gmx.com>
2019-07-23 01:30:12 -0700

    Add BP groupblog integration into group creation process.
    Also adds filter switches to toggle integration into the blog and group
    creation pages.

--- includes/buddypress.php
+++ includes/buddypress.php
@@ -14,6 +14,24 @@
  */
 $enable_groups = apply_filters( 'cac_cc_enable_buddypress_groups', false );
 
+/**
+ * Filter to enable integration into BP blog creation process.
+ *
+ * @since 0.1.0
+ *
+ * @param bool $retval Defaults to true.
+ */
+$enable_blog_creation = apply_filters( 'cac_cc_enable_blog_creation', true );
+
+/**
+ * Filter to enable integration for BP Groupblog into group creation process.
+ *
+ * @since 0.1.0
+ *
+ * @param bool $retval Defaults to true.
+ */
+$enable_groupblog = apply_filters( 'cac_cc_enable_groupblog', true );
+
 // Groups.
 if ( true === $enable_groups && bp_is_groups_component() ) {
 	// Group creation and settings.
@@ -30,7 +48,11 @@ if ( true === $enable_groups && bp_is_groups_component() ) {
 	}
 }
 
-// Blog create.
-if ( is_user_logged_in() && bp_is_create_blog() ) {
+// Blog create (also handles BP Groupblog).
+if ( is_user_logged_in() &&
+	( ( bp_is_create_blog() && true === $enable_blog_creation ) ||
+		( bp_is_group_create() && bp_is_action_variable( 'group-blog', 1 ) && true === $enable_groupblog )
+	)
+) {
 	require __DIR__ . '/frontend-buddypress-blog-create.php';
 }
